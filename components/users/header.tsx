e111bc0c0ffba474e29ecc8ebca3dbcb646a7688
Javier Rodero Álvarez <javierr@ocu.es>
2019-05-09 16:50:36 +0200

    commit 1

---
+++ components/users/header.tsx
@@ -0,0 +1,11 @@
+import '../../styles/global-styles.css';
+const styles = require('./header.css');
+
+export const Header = () => (
+    <tr>
+      <th className="blue-box">Avatar</th>
+      <th className={styles.purpleBox}>Id</th>
+      <th>Name</th>
+    </tr>
+  )
+  
---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import * as Next from 'next';
+import Link from 'next/link';
+import {getUsers} from '../rest-api/github';
+import {User} from '../model/user';
+import {Table} from '../components/users';
+
+interface Props {
+    users: User[];
+}
+
+const Index: Next.NextFunctionComponent<Props> = (props) => (
+    <div>
+        <p>Hello Next</p>
+        <Table users={props.users} />
+        <Link href="/user-info">
+            <a>
+                Navigate to User Info
+            </a>
+        </Link>
+    </div>
+);
+
+Index.getInitialProps = async () => {
+    const users = await getUsers();
+    console.log(users);
+
+    return {
+        users,
+    }
+};
+
+export default Index;
